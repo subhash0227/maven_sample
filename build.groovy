a2b2df898ebe2fd9b88b25a27314e3a53aa097de
Bommasani Vamsi Krishna <bommasani@Bommasanis-MBP.lan>
2023-05-08 22:36:01 -0500

    added jfrog

--- build.groovy
+++ build.groovy
@@ -1,42 +1,53 @@
 pipeline {
-agent any
-options {
+  agent any
+  options {
     buildDiscarder(logRotator(numToKeepStr:'2' , artifactNumToKeepStr: '2'))
     timestamps()
     }
   stages {
-    stage('CheckOut') {
+    stage('SCM') {
       steps {
+        cleanWs()
         echo 'Checking out project from Bitbucket....'
-          git branch: 'main', url: 'git@github.com:vamsi8977/maven_sample.git'
+        git branch: 'main', url: 'git@github.com:vamsi8977/maven_sample.git'
       }
     }
-stage('build') {
+    stage('Build') {
       steps {
         ansiColor('xterm') {
           echo 'Maven Build....'
-           sh "mvn clean install"
+          sh "mvn clean install"
         }
       }
     }
-stage('SonarQube') {
-    steps {
+    stage('SonarQube') {
+      steps {
         withSonarQubeEnv('SonarQube') {
-            sh "mvn sonar:sonar"
+          sh "mvn sonar:sonar -Dsonar.projectKey=maven -Dsonar.projectName='maven_sample'"
         }
-    }
-}
-  }//end stages
-post {
-      success {
-          archiveArtifacts artifacts: "target/*.jar"
-          echo "The build passed."
-      }
-      failure {
-          echo "The build failed."
-      }
-      cleanup{
-        deleteDir()
       }
     }
+    stage('JFrog') {
+      steps {
+        ansiColor('xterm') {
+          sh '''
+            jf rt u target/javaparser-maven-sample-1.0-SNAPSHOT.jar maven/
+            jf scan target/*.jar --fail-no-op --build-name=maven --build-number=$BUILD_NUMBER
+          '''
+        }
+      }
+    }
+  }
+  post {
+    success {
+      archiveArtifacts artifacts: "target/*.jar"
+      echo "The build passed."
+    }
+    failure {
+      echo "The build failed."
+    }
+    cleanup {
+      deleteDir()
+    }
+  }
 }
